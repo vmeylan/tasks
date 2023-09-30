68974c33b1b82f486e3d68a9c42897864ccc3c67
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-09-30 17:46:14 +0200

    features: setup aave tree structure, add setup.sh, requirements.txt

---
+++ aave/csv_writer.py
@@ -0,0 +1,4 @@
+def write_to_csv(data, filename):
+    # Write data to CSV file here
+    pass
+
---
+++ aave/data_processor.py
@@ -0,0 +1,8 @@
+def aggregate_daily_data(raw_data):
+    # Extract and aggregate daily data here
+    return {}
+
+def compute_daily_rate(daily_data):
+    # Compute daily rates here
+    return {}
+
