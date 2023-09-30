fe3f19db681bf82259b525645d12aacd9c80ec3e
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-10-01 00:47:45 +0200

    features: hardcode uni v3 fee tiers (bps)

--- src/uniswap/uniswap.py
+++ src/uniswap/uniswap.py
@@ -28,7 +28,6 @@ def get_v2_pool_address(token0_address=TOKEN_CONTRACT_MAP['WETH'], token1_addres
 def get_v3_pool_addresses(token0_address=TOKEN_CONTRACT_MAP['USDC'], token1_address=TOKEN_CONTRACT_MAP['WETH'], fee_tiers=UNISWAP_V3_TIERS_BPS):
     # https://docs.uniswap.org/contracts/v3/reference/core/UniswapV3Factory
     # https://etherscan.io/address/0x7858e59e0c01ea06df3af3d20ac7b0003275d4bf USDC-USDT example
-    # The pool's fee in hundredths of a bip, i.e. 1e-6, e.g. 500
     v3_factory = w3.eth.contract(address=UNISWAP_V3_FACTORY_ADDR, abi=load_abi(f"{ROOT_DIRECTORY}/src/uniswap/UNISWAP_V3_FACTORY_ABI.json"))
     return [v3_factory.functions.getPool(token0_address, token1_address, fee).call() for fee in fee_tiers]
 
