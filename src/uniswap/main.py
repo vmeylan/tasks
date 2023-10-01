bfc7211132cd6d3e9278c429c5d1710e77ab717e
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-10-01 11:18:07 +0200

    features: implement remaining Uni v3 fields

--- src/uniswap/main.py
+++ src/uniswap/main.py
@@ -1,11 +1,10 @@
 from src.uniswap.constants import TOKEN_CONTRACT_MAP
 from uniswap import *
-from coingecko import get_usd_price
 from utils import write_to_csv
 
 
 def main():
-    token0_address = TOKEN_CONTRACT_MAP['USDC']
+    token0_address = TOKEN_CONTRACT_MAP['WETH']
     token1_address = TOKEN_CONTRACT_MAP['USDC']
     # Fetching pool addresses
     v2_pool_address = get_v2_pool_address(token0_address, token1_address)
@@ -18,17 +17,7 @@ def main():
 
     pool_data.append(fetch_pool_data(v2_pool_address, 'v2'))
 
-    # Get token prices from Coingecko
-    token0_usd_price = get_usd_price(token0)
-    token1_usd_price = get_usd_price(token1)
-
-    # Compile data (this is a placeholder)
-    data = {
-        'Uniswap version': [],
-        'Pool address': [],
-    }
-
-    write_to_csv(data)
+    # write_to_csv(data)
 
 
 if __name__ == "__main__":
--- src/uniswap/uniswap.py
+++ src/uniswap/uniswap.py
@@ -40,6 +40,52 @@ def get_v3_pool_balances(pool_address, token0_address, token1_address):
     return token0_balance, token1_balance
 
 
+def sqrt_price_to_price(sqrtPriceX96, token0Decimals, token1Decimals):
+    """Converts the sqrt price to actual price."""
+    # https://blog.uniswap.org/uniswap-v3-math-primer
+    math_price = (Decimal(sqrtPriceX96) ** 2) / (Decimal(2) ** 192)
+    decimal_adjustment = Decimal(10) ** (token0Decimals - token1Decimals)
+    price = math_price * decimal_adjustment
+    return float(price)
+
+
+def get_pool_level_prices(token0_balance, token1_balance, token0_decimals, token1_decimals, pool_contract, quote_token="token1"):
+    """
+    Calculate the pool level price of token0 quoted by token1 and vice versa.
+
+    Parameters:
+    - token0_balance: Balance of token0 in the pool.
+    - token1_balance: Balance of token1 in the pool.
+    - pool_contract: The pool contract object.
+    - method: The method to compute price ("average" or "spot"). Defaults to "average".
+    - quote_token: Which token to use as the quote currency ("token1" or "token0"). Defaults to "token1".
+
+    Returns:
+    A tuple (priceToken0inToken1, priceToken1inToken0)
+    """
+
+    # Ensure precision for Decimal calculations
+    getcontext().prec = 100
+
+    # Assert non-zero balances
+    assert token0_balance > 0, "Token0 balance must be greater than 0"
+    assert token1_balance > 0, "Token1 balance must be greater than 0"
+
+    sqrtPriceX96 = pool_contract.functions.slot0().call()[0]
+    assert sqrtPriceX96 > 0, "sqrtPriceX96 should be greater than 0"
+
+    # Use the helper to convert sqrt price to actual price
+    priceToken0inToken1 = sqrt_price_to_price(sqrtPriceX96, token0_decimals, token1_decimals)
+    priceToken1inToken0 = 1 / priceToken0inToken1
+
+    if quote_token == "token1":
+        return priceToken0inToken1
+    elif quote_token == "token0":
+        return priceToken1inToken0
+    else:
+        raise ValueError("Invalid quote_token specified. Must be 'token1' or 'token0'.")
+
+
 def get_v3_pool_details(pool_address):
     # Create a contract object using Web3
     pool_contract = w3.eth.contract(address=pool_address, abi=UNISWAP_V3_POOL_ABI)
@@ -59,19 +105,13 @@ def get_v3_pool_details(pool_address):
 
     # Fetch token balances for given pool
     token0_balance, token1_balance = get_v3_pool_balances(pool_address, token0_address, token1_address)
+
+    # Use our new method to fetch the prices
+    priceToken0inToken1 = get_pool_level_prices(token0_balance, token1_balance, token0_decimals, token1_decimals, pool_contract, quote_token="token1")
+    priceToken1inToken0 = get_pool_level_prices(token0_balance, token1_balance, token0_decimals, token1_decimals, pool_contract, quote_token="token0")
+
     token0_balance = token0_balance / (10 ** token0_decimals)
     token1_balance = token1_balance / (10 ** token1_decimals)
-
-    # Calculate prices using the Q64.96 format
-    sqrtPriceX96 = pool_contract.functions.slot0().call()[0]
-    # Calculate prices using the Q64.96 format
-    sqrtPriceX96_decimal = Decimal(sqrtPriceX96)
-    priceToken0inToken1_decimal = (sqrtPriceX96_decimal ** 2) / (Decimal(2) ** 96)
-    priceToken1inToken0_decimal = (Decimal(2) ** 96) / (sqrtPriceX96_decimal ** 2)
-
-    # Convert Decimals back to float for further processing if necessary
-    priceToken0inToken1 = float(priceToken0inToken1_decimal)
-    priceToken1inToken0 = float(priceToken1inToken0_decimal)
 
     # Fetch the token prices in USD from an external source (placeholder)
     # Replace with your actual implementation
@@ -84,24 +124,19 @@ def get_v3_pool_details(pool_address):
     fee_tier = w3.eth.contract(address=pool_address, abi=UNISWAP_V3_POOL_ABI).functions.fee().call()
 
     # Return data in the format of the CSV headers
-    return [
-        "v3",
-        pool_address,
-        token0_address,
-        token1_address,
-        fee_tier,
-        token0_balance,
-        token1_balance,
-        token0_usd_value,
-        token1_usd_value,
-        priceToken0inToken1,
-        priceToken1inToken0,
-        token0_usd_price,
-        token1_usd_price
-    ]
+    return {
+        "Uniswap version": "v3",
+        "Pool address": pool_address,
+        "Contract address for token0": token0_address,
+        "Contract address for token1": token1_address,
+        "Fee tier (in bps)": fee_tier,
+        "Amount of token0 in the pool (normalized by decimals)": token0_balance,
+        "Amount of token1 in the pool (normalized by decimals)": token1_balance,
+        "Total value locked in the pool as token0 denominated in USD": token0_usd_value,
+        "Total value locked in the pool as token1 denominated in USD": token1_usd_value,
+        "Pool level price of token0 quoted by token1": priceToken0inToken1,
+        "Pool level price of token1 quoted by token0": priceToken1inToken0,
+        "Price of token0 in USD from external API": token0_usd_price,
+        "Price of token1 in USD from external API": token1_usd_price
+    }
 
-
-def fetch_pool_data(pool_address, version):
-    details = {}
-    assert False, "This function is not implemented yet"
-    return details
